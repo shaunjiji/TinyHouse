b0c71b144c93f9b1a391e9a77812303f6e502b17
Shaun Jiji <shaunjiji123@gmail.com>
2023-03-07 16:25:09 +0530

    add build in scripts/package.json

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
-const express = require("express");
+import express from 'express';
 const app = express();
 const port = 9000;
 
-app.get("/", (req, res) => res.send("Hello Again!"));
+const one: number = 1;
+const two: number = 2;
+
+
+
+
+app.get("/", (req, res) => res.send(`1 + 2 = ${one + two}`));
 
 app.listen(port);
 
