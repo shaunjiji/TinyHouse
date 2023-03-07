60a5f7b018cdad41eec2d39bbc358c71ddce7f0f
Shaun Jiji <shaunjiji123@gmail.com>
2023-03-07 15:08:22 +0530

    install typescript; add tsconfig.json file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 9000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello Again!"));
 
 app.listen(port);
 
