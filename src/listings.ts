d3ab64a1ea99c67363ebb963431723e75cad72eb
Shaun Jiji <shaunjiji123@gmail.com>
2023-03-08 13:59:32 +0530

    create interface for listing variable

--- src/listings.ts
+++ src/listings.ts
@@ -1,3 +1,15 @@
+interface Listing {
+    id: string;
+    title: string;
+    image: string;
+    address: string;
+    price: number;
+    numOfGuests: number;
+    numOfBeds: number;
+    numOfBaths: number;
+    rating: number;
+}
+
 export const listings = [
     {
       id: "001",
