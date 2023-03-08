c4157bb84cc959a45df672b65a5dac46268c16ca
Shaun Jiji <shaunjiji123@gmail.com>
2023-03-08 15:43:57 +0530

    implement delete listing route

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import { listings } from "./listings"
+
 const app = express();
 const port = 9000;
 
-const one: number = 1;
-const two: number = 2;
+app.use(bodyParser.json())
+
+const one = 1;
+const two = 2;
 
 app.get("/", (_req, res) => res.send(`1 + 2 = ${one + two}`));
+
 app.get("/listings", (_req,res) => {
     return res.send(listings);
 });
 
+app.post("/delete-listing", (req,res) => {
+    const id: string = req.body.id;
+
+    for (let i = 0; i < listings.length; i++) {
+        if (listings[i].id === id) {
+            return res.send(listings.splice(i, 1))
+        }
+    }
+    return res.send("failed to delete listing")
+})
 
 app.listen(port);
 
