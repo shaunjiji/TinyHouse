76326240ccf32db262ebc36ec42b9756c5a85fa3
Shaun Jiji <shaunjiji123@gmail.com>
2023-03-09 15:56:23 +0530

    create barebones schema

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,14 @@
 import express from 'express';
 import {ApolloServer} from 'apollo-server-express'
-import bodyParser from 'body-parser';
-import { listings } from "./listings"
+
+
 
 const app = express();
 const port = 9000;
 
-const server = new ApolloServer({typeDefs: ``, resolvers: {}})
+const server = new ApolloServer()
 server.applyMiddleware({app, path: '/api'});
 
-app.use(bodyParser.json())
-
-app.get("/listings", (_req,res) => {
-    return res.send(listings);
-});
-
-app.post("/delete-listing", (req,res) => {
-    const id: string = req.body.id;
-
-    for (let i = 0; i < listings.length; i++) {
-        if (listings[i].id === id) {
-            return res.send(listings.splice(i, 1))
-        }
-    }
-    return res.send("failed to delete listing")
-})
 
 app.listen(port);
 
