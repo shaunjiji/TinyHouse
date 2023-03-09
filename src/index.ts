bb9b1162bfb4ea3f9074c1cb77112ffe70e2894a
Shaun Jiji <shaunjiji123@gmail.com>
2023-03-09 16:15:57 +0530

    troubleshoot server start bug

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 import express from 'express';
 import {ApolloServer} from 'apollo-server-express'
-
+import {schema} from "./graphql"
 
 
 const app = express();
 const port = 9000;
 
-const server = new ApolloServer()
-server.applyMiddleware({app, path: '/api'});
+const server = new ApolloServer({schema, plugins: [ApolloServerPluginLandingPageGraphQLPlayground],});
 
-
-app.listen(port);
-
-console.log(`[app]: http://localhost:${port}`);
+server.start().then(() => {server.applyMiddleware({ app, path: '/api' })
+    app.listen(port);
+    console.log(`[app]: http://localhost:${port}`)});
