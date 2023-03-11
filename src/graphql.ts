e79e276e71b83eda68c8faea820365d308e304a9
Shaun Jiji <shaunjiji123@gmail.com>
2023-03-11 10:13:50 +0530

    add Listing fields type

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,6 +1,20 @@
-import {GraphQLObjectType, GraphQLString, GraphQLSchema} from "graphql";
+import { GraphQLID, GraphQLInt, GraphQLObjectType, GraphQLString, GraphQLSchema} from "graphql";
 
 
+const Listing = new GraphQLObjectType({
+    name: "Listing",
+    fields: {
+        id: { type: GraphQLID },
+        title: { type: GraphQLString },
+        image: { type: GraphQLString },
+        address: { type: GraphQLString },
+        price: { type: GraphQLInt },
+        numOfGuests: { type: GraphQLInt },
+        numOfBeds: { type: GraphQLInt },
+        numOfBaths: { type: GraphQLInt },
+        rating: { type: GraphQLInt}
+    }
+})
 const query = new GraphQLObjectType({
     name: 'Query',
     fields: {
