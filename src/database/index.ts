9a78ffc61ec357fb82b7ef9448478559d63c2d08
Shaun Jiji <shaunjiji123@gmail.com>
2023-03-16 14:50:11 +0530

    use mongoclient object to connect to db

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,12 @@
 import {MongoClient} from "mongodb"
 
-export const connectDatabase = () => {
+const user = 'user_001';
+const userPassword = 'LjshLzgOlAdIWDbd';
+const cluster = 'cluster0.zap1enl'
+
+const url = `mongodb+srv://${user}:${userPassword}@${cluster}.mongodb.net/?retryWrites=true&w=majority`;
+
+export const connectDatabase = async () => {
+    const client = await MongoClient.connect(url);
 
 }
