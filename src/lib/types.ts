1827ae0646d66c11bc91a5dfde21581d5af4f217
Shaun Jiji <shaunjiji123@gmail.com>
2023-03-16 15:54:36 +0530

    create types.ts and implement listing interface

---
+++ src/lib/types.ts
@@ -0,0 +1,12 @@
+import { ObjectId } from "mongodb";
+
+export interface Listing {
+    _id: ObjectId;
+    title: string;
+    image: string;
+    address: string;
+    price: number;
+    numOfGuests: number;
+    numOfBeds: number;
+    rating: number;
+}
