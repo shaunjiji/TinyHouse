c29e5b11fc2b4173f271ef9e57354aee901b1b0f
Shaun Jiji <shaunjiji123@gmail.com>
2023-03-17 13:49:10 +0530

    create deleteListing mutation

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,8 +1,6 @@
 import { IResolvers } from "@graphql-tools/utils";
-
-import { Listing, listings } from '../listings';
-import { Booking, bookings } from "../bookings";
 import { Database } from "../lib/types";
+import { ObjectId } from "mongodb";
 
 
 export const resolvers: IResolvers = {
@@ -12,43 +10,15 @@ export const resolvers: IResolvers = {
         }
     },
     Mutation: {
-        deleteListing: (_root: undefined, {id}: {id: string}) => {
-            for (let i = 0; i < listings.length; i++) {
-                if (listings[i].id === id) {
-                    return listings.splice(i, 1)[0];
-                }
+        deleteListing:async (_root: undefined, {id}: { id: string }, { db }: { db: Database }) => {
+            const deleteRes = await db.listings.findOneAndDelete({
+                _id: new ObjectId(id)
+            })
+
+            if(!deleteRes.value){
+                throw new Error('failed to delete listing');
             }
-            throw new Error("failed to delete listing")
-        },
-        createBooking: (_root: undefined, {id, timestamp}: {id:string, timestamp: string}) => {
-          for (let i = 0; i < listings.length; i++){
-            if (listings[i].id === id){
-                let numOfBookings = listings[i].bookings.length;
-                const newBooking: Booking = {
-                    id: numOfBookings.toString(),
-                    title: listings[i].title,
-                    image: listings[i].image,
-                    address: listings[i].address,
-                    timestamp
-                };
-                bookings.push(newBooking);
-                listings[i].bookings.push(newBooking.id);
-                return newBooking;
-            }
-            throw new Error ("failed to create booking")
-          }
-        },
-        favoriteListing: (_root: undefined, {id}: {id: string}) => {
-            for (let i = 0; i < listings.length; i++){
-                if (listings[i].id === id){
-                    listings[i].favorite = !listings[i].favorite;
-                    return listings[i];
-                }
-            }
-            throw new Error ("failed to favorite listing");
+            return deleteRes.value
         }
-    },
-    Listing: {
-        numOfBookings: (listing: Listing): number => listing.bookings.length
     }
 }
