3fb8d00388f1bb37a8bf145023530564bc416180
Shaun Jiji <shaunjiji123@gmail.com>
2023-04-24 12:25:53 -0400

    add shape of user interface

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,18 +3,25 @@ import { Collection, ObjectId } from "mongodb";
 export interface Booking {
     _id: ObjectId;
 }
+
 export interface Listing {
     _id: ObjectId;
-
 }
 
 export interface User {
-    _id: ObjectId;
-
-
+    _id: string;
+    token: string;
+    name: string;
+    avatar: string;
+    contact: string;
+    walletId?: string;
+    income: number;
+    bookings: ObjectId[];
+    listings: ObjectId[];
 }
 
 export interface Database {
+    bookings: Collection<Booking>,
     listings: Collection<Listing>;
     users: Collection<User>
 }
