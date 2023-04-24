546042c635c79a950faf9397f8c87ce6270a7837
Shaun Jiji <shaunjiji123@gmail.com>
2023-04-24 17:40:44 -0400

    create Google function than returns user data

--- src/lib/api/Google.ts
+++ src/lib/api/Google.ts
@@ -7,6 +7,20 @@ const auth = new google.auth.OAuth2(
 );
 
 export const Google = {
-    authUrl: '',
-    logIn: async (code: string) => {}
+    authUrl: auth.generateAuthUrl({
+        access_type: "online",
+        scope: ["https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"]
+    }),
+    logIn: async (code: string) => {
+        const { tokens } = await auth.getToken(code)
+
+        auth.setCredentials(tokens);
+
+        const { data } = await google.people({  version: "v1", auth}).people.get({
+            resourceName: "people/me",
+            personFields: "emailAddresses, names, photos"
+        });
+
+        return { user: data };
+    }
 }
