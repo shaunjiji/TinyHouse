2f4d5356a0f122cee2526f3c69da210da9a6e467
Shaun Jiji <shaunjiji123@gmail.com>
2023-04-24 14:20:19 -0400

    Remove old typedefs and listing resolver folder

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -1,36 +1,7 @@
 import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
-    type Booking {
-        id: ID!
-        title: String!
-        image: String!
-        address: String!
-        timestamp: String!
-    }
-
-    type Listing {
-        id: ID!
-        title: String!
-        image: String!
-        address: String!
-        price: Int!
-        numOfGuests: Int!
-        numOfBeds: Int!
-        numOfBaths: Int!
-        rating: Int!
-        numOfBookings: Int!
-        favorite: Boolean!
-    }
+    type Query { }
     
-    type Query {
-        listings: [Listing!]!
-        bookings: [Booking!]!
-    }
-    
-    type Mutation {
-        deleteListing(id: ID!): Listing!
-        createBooking(id: ID!, timestamp: String!): Booking!
-        favoriteListing(id: ID!): Listing!
-    }
+    type Mutation { }
     `;
