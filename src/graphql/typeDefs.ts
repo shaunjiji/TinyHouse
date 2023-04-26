f603952c4e206c338c3e2d7e2252f52f3b90ffa0
Shaun Jiji <shaunjiji123@gmail.com>
2023-04-26 10:07:13 -0400

    create viewer type

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -1,6 +1,14 @@
 import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
+
+    type Viewer {
+        id: id
+        token: String
+        avatar: String
+        hasWallet: Boolean
+        didRequest: Boolean!
+    }
     
     input LogInInput {
         code: String!
