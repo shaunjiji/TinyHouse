21e3ac5a6243d5c206b341284eb650b4cc9c69f4
Shaun Jiji <shaunjiji123@gmail.com>
2023-04-26 12:27:09 -0400

    work on loginViaGoogle function

--- src/graphql/resolvers/Viewer/index.ts
+++ src/graphql/resolvers/Viewer/index.ts
@@ -5,9 +5,39 @@ import { Database, User, Viewer } from "../../../lib/types"
 import { LogInArgs } from "./types";
 
 
-const logInViaGoogle = async (code: string, token: string, db: Database): Promise<User> | undefined => {
-
+const logInViaGoogle = async (
+    code: string, 
+    token: string, 
+    db: Database): 
+    Promise<User> | undefined => {
+    const { user } = await Google.logIn(code);
     
+    if (!user) {
+        throw new Error("Google login error")
+    }
+
+    // Name/Photo/Email Lists
+    const userNamesList = user.names && user.names.length ? user.names : null;
+    const userPhotosList = user.photos && user.photos.length ? user.photos : null;
+    const userEmailsList = user.emailAddresses && user.emailAddresses.length ? user.emailAddresses : null;
+
+    // User Display Name
+    const userName = userNamesList ? userNamesList[0].displayName : null;
+
+    // User Id
+    const userId = userNamesList && userNamesList[0].metadata && userNamesList[0].metadata.source
+    ? userNamesList[0].metadata.source.id
+    : null;
+
+    // User Avatar
+    const userAvatar = userPhotosList && userPhotosList[0].url ? userPhotosList[0].url : null;
+
+    // User Email
+    const userEmail = userEmailsList && userEmailsList[0].value ? userEmailsList[0].value : null;
+
+    if (!userId || !userName || !userAvatar || !userEmail) {
+        throw new Error("Google login error");
+      }
 }
 
 export const viewerResolvers: IResolvers = {
